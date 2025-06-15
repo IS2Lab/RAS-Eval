7c2bc9da7b97cd3621fcf82b0d646f00f47ad612
lanzer-tree <lanzertree@gmail.com>
2025-06-15 11:04:30 +0800

    update tool parser

--- utils/benchmark_utils.py
+++ utils/benchmark_utils.py
@@ -2,6 +2,7 @@ import ast
 import json
 import os
 import sys
+from langchain_core.utils.function_calling import convert_to_openai_function
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -40,20 +41,135 @@ def parse_toolkits(toolkit_paths: list[str]) -> dict[str, int]:
                 names=[ast.alias(name=tool_name)], 
                 level=0
             ))
-            print(ast.unparse(framework_dependencies[-1]))
+        langgraph_dependencies = [
+            ast.ImportFrom(
+                module="langchain_core.tools",
+                names=[ast.alias(name="tool")], 
+                level=0
+            )
+        ]
+        mcp_dependencies = [
+            ast.ImportFrom(
+                module="mcp.server.fastmcp",
+                names=[ast.alias(name="FastMCP")], 
+                level=0
+            )
+        ]
         langgraph_code = ""
         mcp_code = ""
-        for dependency in framework_dependencies:
+        for dependency in langgraph_dependencies + framework_dependencies + dependencies:
             langgraph_code += ast.unparse(dependency) + "\n"
+        for dependency in mcp_dependencies + framework_dependencies + dependencies:
             mcp_code += ast.unparse(dependency) + "\n"
+
+        mcp_code += f"\n\nmcp = FastMCP('{toolkit_name}')\n"
+        langgraph_code += "\n\n"
+
+        for command in commands:
+            mcp_code += ast.unparse(command) + "\n"
+            langgraph_code += ast.unparse(command) + "\n"
+        
+        mcp_code += "\n\n"
+        langgraph_code += "\n\n"
+
+        tool_names: list[str] = []
+        for tool in tools:
+            tool_names.append(tool.name)
+
+            langgraph_function = ast.FunctionDef(
+                name=f"_{tool.name}",
+                args=tool.args,
+                body=[tool.body[0]]+[
+                    ast.Return(
+                        value=ast.Call(
+                            func=ast.Name(id=tool.name, ctx=ast.Load()),
+                            args=[],
+                            keywords=[ast.keyword(arg=arg.arg, value=ast.Name(id=arg.arg, ctx=ast.Load())) for arg in tool.args.args]
+                        )
+                    )
+                ],
+                # decorator_list=[],
+                decorator_list=[
+                    ast.Call(
+                        func=ast.Name(id="tool", ctx=ast.Load()),
+                        args=[],
+                        keywords=[ast.keyword(arg="parse_docstring", value=ast.Constant(value=True))]
+                    )
+                ],
+                returns=tool.returns,
+                type_params=[]
+            )
+            mcp_function = ast.FunctionDef(
+                name=f"_{tool.name}",
+                args=tool.args,
+                body=[tool.body[0]]+[
+                    ast.Return(
+                        value=ast.Call(
+                            func=ast.Name(id=tool.name, ctx=ast.Load()),
+                            args=[],
+                            keywords=[ast.keyword(arg=arg.arg, value=ast.Name(id=arg.arg, ctx=ast.Load())) for arg in tool.args.args]
+                        )
+                    )
+                ],
+                decorator_list=[
+                    ast.Call(
+                        func=ast.Attribute(value=ast.Name(id="mcp", ctx=ast.Load()), attr="tool", ctx=ast.Load()),
+                        args=[],
+                        keywords=[]
+                    )
+                ],
+                returns=tool.returns,
+                type_params=[]
+            )
+            langgraph_code += ast.unparse(ast.fix_missing_locations(langgraph_function)) + "\n\n\n"
+            mcp_code += ast.unparse(ast.fix_missing_locations(mcp_function)) + "\n\n\n" 
+
+        main_body = ast.If(
+            test=ast.Compare(
+                left=ast.Name(id="__name__", ctx=ast.Load()),
+                ops=[ast.Eq()],
+                comparators=[ast.Constant(value="__main__")]
+            ),
+            body=[
+                ast.Expr(
+                    value=ast.Call(
+                        func=ast.Attribute(value=ast.Name(id="mcp", ctx=ast.Load()), attr="run", ctx=ast.Load()),
+                        args=[ast.Constant(value="stdio")],
+                        keywords=[]
+                    )
+                )
+            ],
+            orelse=[]
+        )
+        mcp_code += ast.unparse(ast.fix_missing_locations(main_body)) + "\n"
+        
         if not os.path.exists(f"{path}/langgraph"):
             os.makedirs(f"{path}/langgraph")
         if not os.path.exists(f"{path}/mcp"):
             os.makedirs(f"{path}/mcp")
+        if not os.path.exists(f"{path}/json"):
+            os.makedirs(f"{path}/json")
         with open(f"{path}/langgraph/{toolkit_name}.py", "w") as file:
             file.write(langgraph_code)
         with open(f"{path}/mcp/{toolkit_name}.py", "w") as file:
             file.write(mcp_code)
+
+        json_functions = []
+        for tool_name in tool_names:
+            try:
+                local_vars = {} 
+                exec(f"from src.toolkits.real.{toolkit_name}.langgraph.{toolkit_name} import _{tool_name}", globals(), local_vars) 
+                exec(f"tool = _{tool_name}", globals(), local_vars) 
+                tool = local_vars.get('tool') 
+                json_function = convert_to_openai_function(tool)
+                json_function["name"] = tool_name
+                json_functions.append(json_function)
+            except Exception as e:
+                print(f'Error compiling tool {tool_name}: {e}')
+
+        with open(f"{path}/json/{toolkit_name}.json", "w") as file:
+            file.write(json.dumps(json_functions, indent=4))
+        count[toolkit_name] = len(tool_names)
 
     # Check compiled result
     for path in toolkit_paths:
@@ -64,14 +180,12 @@ def parse_toolkits(toolkit_paths: list[str]) -> dict[str, int]:
         with open(f"{path}/langgraph/{toolkit_name}.py", "r") as file:
             code = file.read()
             exec(code)
-        with open(f"{path}/mcp/{toolkit_name}.py", "r") as file:
-            code = file.read()
-            exec(code)
+            
+        # with open(f"{path}/mcp/{toolkit_name}.py", "r") as file:
+        #     code = file.read()
+        #     exec(code)
+    return count
         
-
-temp = """
-from ..function.toolkit import tool
-"""
 
 def main():
     toolkit_paths = [
@@ -92,7 +206,7 @@ def main():
         "src/toolkits/real/weather_toolkit",
     ]
     count = parse_toolkits(toolkit_paths)
-    # print(ast.dump(ast.parse(temp), indent=4))
+    print(count)
 
 
 if __name__ == "__main__":
